85d69abab7b76dc4c28bb3049c9f0aef1265bf6d
CAUCHY maxime <macauchy@student.42.fr>
2025-12-10 17:32:31 +0100

    feat: set up PostgreSQL and Redis services in Docker Compose
    - Add PostgreSQL service with environment variables and port exposure for local development in docker-compose.yml.
    - Configure Redis service with port exposure for local development in docker-compose.yml.
    - Update docker-compose.override.yml to include PostgreSQL and Redis services for development.
    - Modify docker-compose.prod.yml to set PostgreSQL password from environment variable.
    - Simplify init.sql by removing user creation statements, focusing on database creation.
    - Enhance init_vault.sh to configure PostgreSQL database secrets engine and dynamic roles for services.

--- infra/db/init.sql
+++ infra/db/init.sql
@@ -1,13 +1,7 @@
 -- init script db
 
--- Create users for each service
-CREATE USER "user_service" WITH PASSWORD 'user_service_pass';
-CREATE USER "auth_service" WITH PASSWORD 'auth_service_pass';
-CREATE USER "game_service" WITH PASSWORD 'game_service_pass';
-CREATE USER "chat_service" WITH PASSWORD 'chat_service_pass';
-
 -- Create databases for each service with respective owners
-CREATE DATABASE "user_db" OWNER "user_service";
-CREATE DATABASE "auth_db" OWNER "auth_service";
-CREATE DATABASE "game_db" OWNER "game_service";
-CREATE DATABASE "chat_db" OWNER "chat_service";
+CREATE DATABASE "user_db";
+CREATE DATABASE "auth_db";
+CREATE DATABASE "game_db";
+CREATE DATABASE "chat_db";
